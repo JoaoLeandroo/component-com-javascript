16b9e75fdc10cf0a93486e1c809b0868f9080235
joaoleandro <new.joaoleandro@gmail.com>
2023-05-19 15:36:58 -0300

    modificado o component, agora é uma DIV

--- Component.js
+++ Component.js
@@ -6,16 +6,33 @@ class Card extends HTMLElement {
         const shadow = this.attachShadow({ mode: 'open' })
 
         // Criando O component
-        const componentRoot = document.createElement('h1')
+        const componentRoot = document.createElement('div')
         componentRoot.setAttribute("class", 'component')
-        componentRoot.textContent = "Hello World!"
+        componentRoot.innerHTML = `
+            <h2>Hello World</h2>
+        `
 
         // Criando o Style do component
         const style = document.createElement('style')
         style.textContent = `
             .component {
+                width: 150px;
+                height: 150px;
+                text-align: center;
+                margin: 5px;
+                background-color: #fff;
+                display: flex;
+                align-itens: center;
+            }
+
+            .component:hover {
+                background-color: #c3c3c3;
+            }
+
+            h2 {
                 color: blue;
                 font-family: verdana;
+                margin: auto;
             }
         `
         shadow.appendChild(componentRoot)
